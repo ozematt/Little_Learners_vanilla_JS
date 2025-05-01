f597f441b7249837186f074b78d07b1b4d204c6f
Mateusz Obirek <neutralnie7@gmail.com>
2025-05-01 12:50:18 +0200

    feat: restructure navigation and implement dynamic routing with new page templates

---
+++ src/data/navLinks.js
@@ -0,0 +1,25 @@
+export const navLinks = {
+  home: { href: "/", text: "Home", html: "pages/home.html" },
+  "about-us": {
+    href: "/about-us",
+    text: "About Us",
+    html: "/pages/about-us.html",
+  },
+  academics: {
+    href: "/academics",
+    text: "Academics",
+    html: "/pages/academics.html",
+  },
+  admissions: {
+    href: "/admissions",
+    text: "Admissions",
+    html: "pages/admissions.html",
+  },
+  "student-life": {
+    href: "/student-life",
+    text: "Student Life",
+    html: "pages/student-life.html",
+  },
+  contact: { href: "/contact", text: "Contact", html: "pages/contact.html" },
+  404: { href: "/404", text: "404", html: "/pages/404.html" },
+};
